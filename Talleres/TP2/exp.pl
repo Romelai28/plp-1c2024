484f5fc03a285fa10517b7a7f4e7703d516c23b4
Mauricio Romero Laino <Romelai28@gmail.com>
2024-06-22 06:19:47 -0300

    comentario

--- Talleres/TP2/exp.pl
+++ Talleres/TP2/exp.pl
@@ -1,9 +1,9 @@
 % Completar especificación de instanciaciones de parametros.
 n_filas(T, F) :- length(T, F).
 
+
 % Completar especificación de instanciaciones de parametros.
 n_columnas([Fila|_], C) :- length(Fila, C).
-
 
 
 % Ejercicio 1:
@@ -14,6 +14,7 @@ n_columnas([Fila|_], C) :- length(Fila, C).
 tablero(0, C, []).
 tablero(F, C, [Fila|Res]) :- F>0, F2 is F-1, crearFila(C, Fila), tablero(F2, C, Res).
 
+
 % crearFila(+Largo, ?Lista)
 % Si Lista no esta instanciada, devuelve true sii la lista tiene longitud igual al Largo
 crearFila(0, []).
@@ -21,8 +22,9 @@ crearFila(Largo, [X|Resto]) :- Largo > 0, Largo2 is Largo -1, crearFila(Largo2, 
 
 
 %% Ejercicio 2
-%% ocupar(+Pos,?Tablero) será verdadero cuando la posición indicada esté ocupada.
+%% ocupar(+Pos, ?Tablero) será verdadero cuando la posición indicada esté ocupada.
 ocupar(pos(X,Y), T) :- nth0(X, T, Fila), nth0(Y, Fila, ocupado).  % ocupar indirectamente checkea que este enRango por el nth0.
+
 
 % ejemplo:
 tablero(ej5x5, T) :-
@@ -41,17 +43,20 @@ vecino(pos(X,Y), T, V) :- X1 is X-1, enRango(pos(X1,Y), T), V = pos(X1, Y).  % C
 vecino(pos(X,Y), T, V) :- Y1 is Y+1, enRango(pos(X,Y1), T), V = pos(X, Y1).  % Caso me voy derecha.
 vecino(pos(X,Y), T, V) :- Y1 is Y-1, enRango(pos(X,Y1), T), V = pos(X, Y1).  % Caso me voy izquierda.
 
+
 % enRango(+Pos, ?T)
 % Cuando T no esta instanciado, genera infinitos (no todos) tableros en los que X, Y esten en rango.
 enRango(pos(X,Y), [T|TS]) :- length([T|TS], Long_X), 0 =< X, X < Long_X,
                              length(T, Long_Y), 0=< Y, Y < Long_Y.
+
 
 %% Ejercicio 4
 %% vecinoLibre(+Pos, +Tablero, -PosVecino) idem vecino/3 pero además PosVecino
 %% debe ser una celda transitable (no ocupada) en el Tablero
 vecinoLibre(pos(X,Y), T, V) :- vecino(pos(X,Y), T, V), estaLibre(V, T).
 
-% estaLibre agregar especificación de los inputs!
+
+% Completar especificación de instanciaciones de parametros.
 estaLibre(pos(X,Y), T) :- nth0(X, T, Fila), nth0(Y, Fila, Elem), not(atom(Elem)).
 
 
@@ -75,22 +80,27 @@ caminoAux(Inicio, Fin, T, Visitados, [V|Camino]) :-
     not(member(V, Visitados)),
     caminoAux(V, Fin, T, [V|Visitados], Camino).
 
+
 %% Ejercicio 6
 %% camino2(+Inicio, +Fin, +Tablero, -Camino) ídem camino/4 pero que las soluciones
 %% se instancien en orden creciente de longitud.
 camino2(Inicio , Fin, Tablero, Camino) :-
     n_filas(Tablero, F), n_columnas(Tablero, C),
-    Techo is (F*C), camino2Aux(Inicio, Fin, Tablero, Camino, Techo, Techo).
+    Techo is (F*C), camino2Aux(Inicio, Fin, Tablero, Camino, 0, Techo).
 
-camino2Aux(Inicio, Fin, Tablero, Camino, Iterador, Techo) :-
-    0=< Iterador, Iterador =< Techo,
-    It2 is Iterador-1,
-    camino2Aux(Inicio, Fin, Tablero, Camino, It2, Techo).
 
+% Completar especificación de instanciaciones de parametros.
 camino2Aux(Inicio, Fin, Tablero, Camino, Iterador, Techo) :-
     0=< Iterador, Iterador =< Techo,
     caminoLongFija(Inicio, Fin, Tablero, Camino, Iterador).
 
+camino2Aux(Inicio, Fin, Tablero, Camino, Iterador, Techo) :-
+    0=< Iterador, Iterador =< Techo,
+    It2 is Iterador+1,
+    camino2Aux(Inicio, Fin, Tablero, Camino, It2, Techo).
+
+
+% Completar especificación de instanciaciones de parametros.
 caminoLongFija(Inicio, Fin, Tablero, Camino, Longitud) :- camino(Inicio, Fin, Tablero, Camino), length(Camino, N), N =:= Longitud.
 
 
@@ -106,6 +116,8 @@ caminoDual(Inicio, Fin, Tablero1, Tablero2, Camino) :-
     unionTableros(Tablero1, Tablero2, T),
     camino(Inicio, Fin, T, Camino).
 
+
+% Completar especificación de instanciaciones de parametros.
 unionTableros(T1, T2, TableroRes) :-
     n_filas(T1, F1), n_filas(T2, F2),
     F is min(F1, F2),
