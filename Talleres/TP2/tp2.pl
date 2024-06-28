387e93c715c7bb3b633fb1979078279ce7e550c9
Mauricio Romero Laino <Romelai28@gmail.com>
2024-06-28 15:52:48 -0300

    tests

--- Talleres/TP2/tp2.pl
+++ Talleres/TP2/tp2.pl
@@ -41,6 +41,8 @@ crearFila(Largo, [_|Resto]) :- Largo > 0, Largo2 is Largo-1, crearFila(Largo2, R
 ocupar(pos(X,Y), T) :- nonvar(T), nth0(X, T, Fila), nth0(Y, Fila, ocupado).  % ocupar indirectamente verifica que Pos este enRango por los nth0.
 ocupar(pos(X,Y), T) :- var(T), X>=0, Y>=0, desde(2, Suma), paresNatQueSuman(F, C, Suma), tablero(F, C, T), ocupar(pos(X,Y), T).
 %% (Como en nuestro caso de uso el Suma no esta instanciado, usamos desde y no desde2 visto en clase)
+%% Dividimos en casos:
+%% Cuando T viene instanciado, unifica el elemento de la posición Pos del Tablero con ocupado.
 %% Cuando T viene sin instanciar, ocupar crear los tableros de todos los tamaños con la casilla Pos ocupada.
 
 
@@ -61,6 +63,9 @@ vecino(pos(X,Y), T, pos(X1, Y)) :- X1 is X+1, enRango(pos(X1,Y), T).  % Caso me 
 vecino(pos(X,Y), T, pos(X1, Y)) :- X1 is X-1, enRango(pos(X1,Y), T).  % Caso me voy arriba.
 vecino(pos(X,Y), T, pos(X, Y1)) :- Y1 is Y+1, enRango(pos(X,Y1), T).  % Caso me voy derecha.
 vecino(pos(X,Y), T, pos(X, Y1)) :- Y1 is Y-1, enRango(pos(X,Y1), T).  % Caso me voy izquierda.
+
+%% Podríamos considerar vecino, y luego por lo tanto, vecinoLibre Generate&Test ya que genera todo el espacio de soluciones posibles (los 4 casilleros adyacentes)
+%% y testea si son válidos / válidos y libres, respectivamente.
 
 
 %% Ejercicio 4
@@ -97,8 +102,9 @@ camino(Inicio, Fin, Tablero, [Inicio|Camino]) :-
     estaLibre(Inicio, Tablero), estaLibre(Fin, Tablero),  % estaLibre indirectamente verifica que este enRango por el nth0.
     caminoAux(Inicio, Fin, Tablero, [Inicio], Camino).
 
-%% COMPLETAR!!!!!!!!!!!!!!!!!!!!!!!
+%% caminoAux(+Inicio, ?Fin, +Tablero, +Visitados, ?Camino)
 %% caminoAux nos permite tener una lista auxiliar con las posiciones visitadas.
+%% Inicio debe venir instanciado porque es usado en vecinoLibre en el primer argumento.
 caminoAux(Pos, Pos, _, _, []).  % Que Pos sea valida fue verificado en camino/4 por estaLibre
 caminoAux(Inicio, Fin, Tablero, Visitados, [V|Camino]) :-
     vecinoLibre(Inicio, Tablero, V),
@@ -138,6 +144,8 @@ camino2(Inicio , Fin, Tablero, Camino) :-
 % Cuando Camino esta instanciado y Longitud no esta instanciado, instancia Longitud en la longitud del camino. (Falla si el camino no era válido)
 caminoLongFija(Inicio, Fin, Tablero, Camino, Longitud) :- camino(Inicio, Fin, Tablero, Camino), length(Camino, Longitud).
 
+%% La implamentacion de caminoLongFija es generate&Test porque utiliza el esquema, al generar todos los caminos candidatos y filtrar por la longitud
+
 
 %% 6.1. Analizar la reversibilidad de los parámetros Inicio y Camino justificando adecuadamente en
 %% cada caso por qué el predicado se comporta como lo hace.
@@ -152,7 +160,7 @@ caminoLongFija(Inicio, Fin, Tablero, Camino, Longitud) :- camino(Inicio, Fin, Ta
 %   va a llamar a camino que le va a generar todos los caminos para toda posicion libre que instancia como inicio en estaLibre(inicio, tablero)
 %   hasta el fin que le pasa caminoLongFija y luego va a chequear cuales son los que cumplen la longitud.
 %   Si camino no esta instanciado, e inicio si. En ese caso camino2 va a para cada L generada con 
-%   el betweeen(1, Techo) va a entrar a caminoLongFija que va a llamar a Camino que va a generar todos los caminos que inicien en Inicio
+%   el between(1, Techo) va a entrar a caminoLongFija que va a llamar a Camino que va a generar todos los caminos que inicien en Inicio
 %   y terminen en Fin y se va a quedar con los de longitud L. Va a hacer eso hasta recorrer todos los L y ahi va a terminar una vez que el 
 %   between termine.
 
@@ -177,38 +185,121 @@ caminoOptimo(Inicio, Fin, Tablero, Camino) :-
 caminoDual(Inicio, Fin, Tablero1, Tablero2, Camino) :-
         camino(Inicio, Fin, Tablero1, Camino), camino(Inicio, Fin, Tablero2, Camino).
 
+%% Esta implementacion de caminoDual es Generate&Test, porque primero busca todas las soluciones candidatas que van a ser los caminos
+%% entre Inicio y Fin en el tablero1 y luego filtra cuales de esos caminos valen en el tablero2. Entonces caminoDual utiliza el esquema de 
+%% Generate&Test.
+
 
 %%%%%%%%
 %% TESTS
 %%%%%%%%
 
-cantidadTestsTablero(6). % Actualizar con la cantidad de tests que entreguen
+tablero(ej5x5, T) :-
+    tablero(5, 5, T),
+    ocupar(pos(1, 1), T),
+    ocupar(pos(1, 2), T).
+
+%% tablero(ej5x5, T)
+%% _ _ _ _ _
+%% _ # # _ _
+%% _ _ _ _ _
+%% _ _ _ _ _
+%% _ _ _ _ _
+
+tablero(pared, T) :-
+    tablero(3,3,T),
+    ocupar(pos(0,1), T),
+    ocupar(pos(1,1),T),
+    ocupar(pos(2,1),T).
+
+%% tablero(pared, T)
+%% _ # _
+%% _ # _
+%% _ # _
+
+tablero(ej4x5, T) :-
+    tablero(4,5, T),
+    ocupar(pos(2,1), T),
+    ocupar(pos(1,3), T).
+
+%% _ _ _ _ _
+%% _ _ _ # _
+%% _ # _ _ _
+%% _ _ _ _ _
+
+
+cantidadTestsTablero(5). % Actualizar con la cantidad de tests que entreguen
 testTablero(1) :- tablero(0,0,[]). 
 testTablero(2) :- tablero(2,1,[[_],[_]]).
 testTablero(3) :- tablero(2,3, [[_,_,_],[_,_,_]]).
 testTablero(4) :- ocupar(pos(0,0), [[ocupada]]).
-testTablero(5) :- ocupar(1,1, [[_, _], [_,_]]). 
+testTablero(5) :- ocupar(pos(1,1), [[_, _], [_,_]]). 
 % testTablero(6) :- ocupar(1, 1, [[ocupada, _]]). Hay que negar esto
 
 % Agregar más tests
-cantidadTestsVecino(1). % Actualizar con la cantidad de tests que entreguen
+cantidadTestsVecino(5). % Actualizar con la cantidad de tests que entreguen
 testVecino(1) :- vecino(pos(0,0), [[_,_]], pos(0,1)). % Caso en el que parto de una posición válida y caigo en un vecino válido.
 testVecino(2) :- vecino(pos(0,2), [[_,_]], pos(0,1)). % Caso en el que parto de una posición invalida y caigo en un vecino válido.
 testVecino(3) :- vecino(pos(1,1), [[_,_],[_,_]], pos(2,1)). % Caso en el que parto de una posición válida y caigo en un vecino invalido.
 testVecino(4) :- vecino(pos(3,1), [[_,_],[_,_]], pos(3, 2)). % Caso en el que parto de una posición invalida y caigo en un vecino invalido.
-testVecino(5) :- vecinoLibre(pos(1,1), [[_,_]]).
+testVecino(5) :- vecinoLibre(pos(1,1), [[_,_]], pos(0,1)).
 
 
 % Agregar más tests
 
-cantidadTestsCamino(0). % Actualizar con la cantidad de tests que entreguen
-testCamino(1) :- tablero(ej5x5, T), bagof(Camino, (camino2(pos(0,0), pos(2,3), T, Camino)), CaminoS), length(CaminoS, Longitud), Longitud == 287.
-testCamino(2) :- tablero(ej5x5, T), bagof(Camino, (camino(pos(0,0), pos(2,3), T, Camino)), CaminoS), length(CaminoS, Longitud), Longitud == 287.
+cantidadTestsCamino(10). % Actualizar con la cantidad de tests que entreguen
+testCamino(1) :- tablero(ej5x5, T), bagof(Camino, (camino(pos(0,0), pos(2,3), T, Camino)), CaminoS), length(CaminoS, Longitud), Longitud == 287.
+testCamino(2) :- tablero(pared, T), not(camino(pos(0,0), pos(2,2), T, C)).  %% Camino imposible: porque debe cruzar la pared.
+testCamino(3) :- tablero(2, 2, T), not(camino(pos(1,1), pos(2,2), T, C)).  %% No existe un camino que empiece dentro del tablero y termine en una posición de afuera.
+testCamino(4) :- tablero(2, 2, T), not(camino(pos(3,2), pos(0,0), T, C)).  %% No existe un camino que empiece fuera del tablero y comience en una posición de afuera.
+testCamino(6) :- tablero(ej4x5, T), not(camino(pos(1,3), pos(0,0), T, Camino)).  %% Camino imposible: empieza en una casilla ocupada.
+testCamino(7) :- tablero(ej4x5, T), not(camino(pos(0,0), pos(1,3), T, Camino)).  %% Camino imposible: termina en una casilla ocupada.
+testCamino(8) :- tablero(ej4x5, T), not(camino(pos(2,1), pos(1,3), T, Camino)).  %% Camino imposible: empieza y termina en una casila ocupada.
+testCamino(9) :- tablero(ej4x5, T), not(camino(pos(1,3), pos(1,3), T, Camino)).  %% Camino imposible: camino a si mismo, caso ocupado.
+testCamino(10) :- tablero(ej4x5, T), camino(pos(0,0), pos(0,0), T, [pos(0,0)]).  %% Camino posible: camino a si mismo, caso libre.
+testCamino(11) :- tablero(ej5x5, T), camino(pos(3,3), pos(2,1), T, [pos(3,3), pos(3,2), pos(3,1), pos(2,1)]).  %% Camino optimo.
+testCamino(12) :- tablero(ej5x5, T), camino(pos(3,3), pos(2,1), T, [pos(4,3), pos(4,4), pos(3,4), pos(3,3), pos(3,2), pos(3,1), pos(2,1)]). %% Camino no optimo.
 
-cantidadTestsCaminoOptimo(0). % Actualizar con la cantidad de tests que entreguen
-% Agregar más tests
+testCamino(13) :- tablero(ej5x5, T), bagof(Camino, (camino2(pos(0,0), pos(2,3), T, Camino)), CaminoS), length(CaminoS, Longitud), Longitud == 287.
 
-cantidadTestsCaminoDual(0). % Actualizar con la cantidad de tests que entreguen
+%% Testea que se cumpla la propiedad de camino2 de dar los caminos de manera creciente creciente en longitudes. 
+testCamino(14) :-
+    tablero(ej5x5, T),
+    findall(Long, (camino2(pos(0,0), pos(2,3), T, Camino), length(Camino, Long)), ListaLongitudes),
+    length(ListaLongitudes, Size),
+    Size2 is Size-2,
+    between(0, Size2, I),
+    Iplus1 is I+1,
+    nth0(I, ListaLongitudes, LongCamino),
+    nth0(Iplus1, ListaLongitudes, LongCamino2),
+    LongCamino =< LongCamino2.
+
+testCamino(15) :- tablero(pared, T), not(camino(pos(0,0), pos(2,2), T, C)).  %% Camino imposible: porque debe cruzar la pared.
+testCamino(16) :- tablero(2, 2, T), not(camino(pos(1,1), pos(2,2), T, C)).  %% No existe un camino que empiece dentro del tablero y termine en una posición de afuera.
+testCamino(17) :- tablero(2, 2, T), not(camino(pos(3,2), pos(0,0), T, C)).  %% No existe un camino que empiece fuera del tablero y comience en una posición de afuera.
+testCamino(18) :- tablero(ej4x5, T), not(camino(pos(1,3), pos(0,0), T, Camino)).  %% Camino imposible: empieza en una casilla ocupada.
+testCamino(19) :- tablero(ej4x5, T), not(camino(pos(0,0), pos(1,3), T, Camino)).  %% Camino imposible: termina en una casilla ocupada.
+testCamino(20) :- tablero(ej4x5, T), not(camino(pos(2,1), pos(1,3), T, Camino)).  %% Camino imposible: empieza y termina en una casila ocupada.
+testCamino(21) :- tablero(ej4x5, T), not(camino(pos(1,3), pos(1,3), T, Camino)).  %% Camino imposible: camino a si mismo, caso ocupado.
+testCamino(22) :- tablero(ej4x5, T), camino(pos(0,0), pos(0,0), T, [pos(0,0)]).  %% Camino posible: camino a si mismo, caso libre.
+testCamino(23) :- tablero(ej5x5, T), camino(pos(3,3), pos(2,1), T, [pos(3,3), pos(3,2), pos(3,1), pos(2,1)]).  %% Camino optimo.
+testCamino(24) :- tablero(ej5x5, T), camino(pos(3,3), pos(2,1), T, [pos(4,3), pos(4,4), pos(3,4), pos(3,3), pos(3,2), pos(3,1), pos(2,1)]). %% Camino no optimo.
+
+
+
+cantidadTestsCaminoOptimo(4). % Actualizar con la cantidad de tests que entreguen
+testCaminoOptimo(1) :- tablero(ej5x5, T), caminoOptimo(pos(3,3), pos(2,1), T, Camino), length(Camino, 4).  %% No tiene casillas ocupadas en el camino directo.
+testCaminoOptimo(2) :- tablero(ej5x5, T), caminoOptimo(pos(3,3), pos(2,1), T, [pos(3,3), pos(3,2), pos(3,1), pos(2,1)]).
+
+testCaminoOptimo(3) :- tablero(ej5x5, T), caminoOptimo(pos(1,3), pos(1,0), T, Camino), length(Camino, 6).  %% Tiene que desviarse del camino directo para evitar las casillas ocupadas.
+testCaminoOptimo(4) :- tablero(pared, T), not(caminoOptimo(pos(0,0), pos(2,2), T, C)).                     %% Camino imposible porque debe cruzar la pared.
+
+
+% testCaminoOptimo(5) :- tablero(ej5x5, T), not(camino(pos(1,3), pos(0,0), T, Camino)).
+
+
+cantidadTestsCaminoDual(1). % Actualizar con la cantidad de tests que entreguen
+testCaminoDual(1) :- tablero(ej5x5, T), caminoOptimo(pos(3,3), pos(2,1), T, Camino), length(Camino, 4).
 % Agregar más tests
 
 tests(tablero) :- cantidadTestsTablero(M), forall(between(1,M,N), testTablero(N)).
